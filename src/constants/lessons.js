3a2fcfdd8f3bb2643f75af953d5fba1fede057f7
Serhii Staryk <56160276+SerhiiStaryk@users.noreply.github.com>
2024-08-28 23:23:30 +0300

    Dev (#5)
    * finish main functionality
    
    * change time
    
    * fix timetable
    
    * fix code
    
    * add google analytic
    
    * change timetable
    
    * change icon
    
    * change readme
    
    * change-schedule

---
+++ src/constants/lessons.js
@@ -0,0 +1,29 @@
+const YA_DOSLIDZHUYU_SVIT = 'Я досліджую світ';
+const MATEMATYKA = 'Математика';
+const UKRAYINSKA_MOVA = 'Українська мова';
+const FIZKULTURA = 'Фізкультура';
+const OBRAZOTVORCHE = 'Образотворче';
+const INFORMATYKA = 'Інформатика';
+const ANHLIYSKA_MOVA = 'Англійська мова';
+const KHRYSTYYANSKA_ETYKA = 'Християнська етика';
+const YADS_UKRAYINSKA_MOVA = 'ЯДС (українська мова)';
+const KHOREOHRAFIYA = 'Хореографія';
+const MUZYKA = 'Музика';
+const YADS_PRYRODA  ='ЯДС (природа)';
+const YADS_TRUD_NAV ='ЯДС (труд. нав.)';
+
+export {
+  YA_DOSLIDZHUYU_SVIT,
+  MATEMATYKA,
+  UKRAYINSKA_MOVA,
+  FIZKULTURA,
+  OBRAZOTVORCHE,
+  INFORMATYKA,
+  ANHLIYSKA_MOVA,
+  KHRYSTYYANSKA_ETYKA,
+  YADS_UKRAYINSKA_MOVA,
+  KHOREOHRAFIYA,
+  MUZYKA,
+  YADS_PRYRODA,
+  YADS_TRUD_NAV
+}
