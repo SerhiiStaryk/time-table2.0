95d0d4005d5874eb149df7a789847ebe740f16a5
Serhii Staryk <56160276+SerhiiStaryk@users.noreply.github.com>
2024-09-01 23:27:43 +0300

    Dev (#10)
    * finish main functionality
    
    * change time
    
    * fix timetable
    
    * fix code
    
    * add google analytic
    
    * change timetable
    
    * change icon
    
    * change readme
    
    * change-schedule
    
    * refactor-code
    
    * refactor-code (#7)
    
    * code-improvement
    
    * improve-style

---
+++ src/components/lessons-list/index.jsx
@@ -0,0 +1,20 @@
+import './style.css';
+
+import { DAYS, SHORT_NAME_DAYS } from '../../constants/date';
+import { TIME_TABLE } from '../../constants/schedule';
+import { getCurrentDay } from '../../helpers';
+import ScheduleLessons from '../schedule-lessons';
+
+const LessonsList = ({ group }) => (
+  <div id='lessons-list'>
+    {TIME_TABLE[group].map((day, idx) => (
+      <ScheduleLessons
+        key={idx}
+        item={day}
+        today={DAYS[SHORT_NAME_DAYS[getCurrentDay()]]}
+      />
+    ))}
+  </div>
+);
+
+export default LessonsList;
---
+++ src/components/schedule-bells/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+import { TIME } from '../../constants/date';
+import { getArrFromObj } from '../../helpers';
+import Card from '../card'
+
+const ScheduleBells = () => {
+  const times = getArrFromObj(TIME);
+
+  return (<Card>
+    <h3>Розклад дзвінків</h3>
+    <ol>
+      {times.map((item, index) => (<li key={index} className>{item}</li>))}
+    </ol>
+  </Card>);
+}
+
+export default ScheduleBells;
