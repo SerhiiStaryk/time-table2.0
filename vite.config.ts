f51d2e8ec59d39f4a73489881d91622382cc4410
Serhii Staryk <serhii.staryk@gmail.com>
2025-02-26 01:06:18 +0200

    fix code

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/time-table2.0/',
+  base: 'https://serhiistaryk.github.io/time-table2.0/',
 });
