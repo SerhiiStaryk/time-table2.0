67499b8fe7cd02d57aa07e484ee06f6c6d1cecf8
Serhii Staryk <serhii.staryk@gmail.com>
2025-02-26 08:18:01 +0200

    Fix GitHub Pages routing

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,20 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/Home';
 // import NotFoundPage from './pages/NotFoundPage';
 
-const router = createHashRouter([
-  {
-    path: '/time-table2.0/',
-    element: <Layout />,
-    children: [
-      { path: '', element: <HomePage /> },
-      // { path: 'about', element: <AboutPage /> },
-    ],
-  },
-  // { path: '*', element: <NotFoundPage /> },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/time-table2.0/',
+      element: <Layout />,
+      children: [
+        { path: '', element: <HomePage /> },
+        // { path: 'about', element: <AboutPage /> },
+      ],
+    },
+    // { path: '*', element: <NotFoundPage /> },
+  ],
+);
 
 export default router;
