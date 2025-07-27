064edda3f5268d585c1969d4692a1a331644962e
Serhii Staryk <serhii.staryk@gmail.com>
2025-07-27 13:27:19 +0300

    feat: add PWA support with service worker registration and update vite config
    - Added vite-plugin-pwa to package.json for PWA capabilities.
    - Implemented service worker registration in main.tsx for offline support.
    - Enhanced vite.config.ts with PWA manifest and caching strategies for Google Fonts and app resources.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,74 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/time-table2.0/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icon512_maskable.png', 'icon512_rounded.png'],
+      manifest: {
+        id: '/time-table2.0/',
+        name: 'timetable2.0',
+        short_name: 'timetable2.0',
+        description: 'Розклад уроків',
+        start_url: '/time-table2.0/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        lang: 'uk-UA',
+        icons: [
+          {
+            src: 'icon512_maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon512_rounded.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshots/wide-screenshot1.png',
+            sizes: '1280x720',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+          {
+            src: 'screenshots/narrow-screenshot1.png',
+            sizes: '360x640',
+            type: 'image/png',
+            form_factor: 'narrow',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/serhiistaryk\.github\.io\/time-table2\.0\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'app-cache',
+              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 30 },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
